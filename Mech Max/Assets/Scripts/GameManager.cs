1dc63ca08c5d04d8ca8b34b8a1f5aa43cb09fde5
Abhiram A <abhiram.elf@gmail.com>
2020-03-31 23:12:40 +0530

    Waves added

---
+++ Mech Max/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject firstWave;
+    public GameObject secondWave;
+    public GameObject thirdWave;
+
+    public WaveSet setWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstWave.SetActive(false);
+        secondWave.SetActive(false);
+        thirdWave.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (setWave.firstWaveSet)
+        {
+            firstWave.SetActive(true);
+            secondWave.SetActive(false);
+            thirdWave.SetActive(false);
+        }
+
+        else if (setWave.secondWaveSet)
+        {
+            firstWave.SetActive(false);
+            secondWave.SetActive(true);
+            thirdWave.SetActive(false);
+        }
+
+        else if (setWave.thirdWaveSet)
+        {
+            firstWave.SetActive(false);
+            secondWave.SetActive(false);
+            thirdWave.SetActive(true);
+        }
+    }
+}
